606b942032e8067f2a134dbd2b66c5400a0f784d
Maiada <>
2021-06-03 12:47:37 +0000

    addjs

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var num = prompt("How many series have you watched on our website? ? ");
+console.log(num);
+if (num >= 5) {
+  alert("hello you are one from our family Please tell others about us")
+   document.write("<p style='color:red;'> This became " + num + " series from our website </p>")
+} else if(num < 5) {
+  alert("We hope you have a great time to come back and watch another series")
+   document.write("<p style='color:red;'> This became " + num + " series from our website </p>")
+  }
