cdbeafefc114aae381ac0b3f49f0413c924d6ccb
Maiada <>
2021-06-06 13:17:02 +0000

    addloop

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 
-var num = prompt("How many series have you watched on our website? ? ");
-console.log(num);
-if (num >= 5) {
-  alert("hello you are one from our family Please tell others about us")
-   document.write("<p style='color:red;'> This became " + num + " series from our website </p>")
-} else if(num < 5) {
-  alert("We hope you have a great time to come back and watch another series")
-   document.write("<p style='color:red;'> This became " + num + " series from our website </p>")
+//var num = prompt("How many series have you watched on our website? ? ");
+//console.log(num);
+//if (num >= 5) {
+ // alert("hello you are one from our family Please tell others about us")
+ //  document.write("<p style='color:red;'> This became " + num + " series from our website </p>")
+//} else if(num < 5) {
+ // alert("We hope you have a great time to come back and watch another series")
+ //  document.write("<p style='color:red;'> This became " + num + " series from our website </p>")
+//}
+
+var kindwatch = prompt("How do you prefer to watch your series? Choose if online ,download ")
+
+function kindwatchfun() {
+
+  while (kindwatch != "online" && kindwatch!= "download") {
+
+    kindwatch = prompt("please enter on of : online, download");
   }
+
+ }
+
+kindwatchfun();
+
+
+
+
+var numser = prompt("How many series have you watched on our website?")
+
+var numserfun = function () {
+  var output='';
+  
+  for (var i=0 ; i<numser; i++)
+   {
+   output = output +  "<img src='https://money-h.com/picture//fin-will_1/how-to-encourage-someone-in-3-steps.jpg' width=50px; />"
+  }
+  return output;
+}
+var result = numserfun();
+ document.write(result);
