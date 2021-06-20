e106cc471c98f80a9894b168571bbf06272f2ae5
Maiada-Ibrahim <85097312+Maiada-Ibrahim@users.noreply.github.com>
2021-06-20 20:43:06 +0300

    test

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ while (kindwatch != "online" && kindwatch!= "download") {
  document.write(output);*/
 
   
-var kindwatch = prompt("How do you prefer to watch your series? Choose if online ,download ")
+/*var kindwatch = prompt("How do you prefer to watch your series? Choose if online ,download ")
 
 function kindwatchfun() {
 
@@ -56,7 +56,16 @@ var numserfun = function () {
   return output;
 }
 var result = numserfun();
- document.write(result);
+ document.write(result);*/
+let userName = prompt('what is your name?');
+alert('hi'+userName+'for our web site' );
+let location = prompt('where the most place in jordan think to vist?');
+alert('It is a good option that you are considering a visit'+location+' It is a nice place for entertainment I think you can ask me to go with you '+userName );
+let food = prompt('What is your favorite food?');
+alert('You must try the' + location + 'in the Jordanian way, it is very tasty' );
+let souv = prompt('What will you take from Jordan as a souvenir?');
+alert('It is agood choose' + souv+'You must take a picture of the Jordanian shemagh, it will look beautiful on you and you will not forget this experience'  );
+ var userName = prompt('what is your name');
 
 
 
